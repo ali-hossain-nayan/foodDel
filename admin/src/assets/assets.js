8b6236ed37718b8c6dd057169fcd33e862c07c67
Nayan1234l <ali15-13943@diu.edu.bd>
2025-06-26 12:37:00 +0600

    updated-backend-url

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -375,5 +375,5 @@ export const food_list = [
     },
 ]
 
-// export const url = 'http://localhost:4000'
+// //export const url = 'http://localhost:4000'
 export const url = 'https://khaidai-com-backend.onrender.com'
